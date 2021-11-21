6fe570a02c8546197e08147fb38d333e9bc51857
Prabhu Nath Tiwary <prabhunt@micelio.local>
2021-11-21 21:28:44 +0530

    first

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let r = 7;
-let string = "";
+let pattern = "";
 for (let i = 1; i <= r; i++) {
   for (let j = 0; j < i; j++) {
-    string += "*";
+    pattern += "*";
   }
-  string += "\n";
+  pattern += "\n";
 }
-console.log(string);
+console.log(pattern);
